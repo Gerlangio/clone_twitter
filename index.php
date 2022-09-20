a82227f1c504de1b8f86cf3b2c5d289d5bfab44f
gerlangiosoares <gerlangio@gmail.com>
2022-09-20 15:24:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Twitter.É o que está acontecendo.Twitter</title>
+    <title>Twitter.É o que está acontecendo / Twitter</title>
 </head>
 <body>
     
